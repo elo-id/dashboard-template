38dcdc5d571ed5499a62932a2d4cb0feccc36d15
Raphaelle <raveraph74@gmail.com>
2025-06-30 13:58:31 +0700

    template dashboard

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'prismjs/themes/prism-okaidia.css'; // or prism-dark.css
+import Prism from 'prismjs';
+
+Vue.directive('prism', {
+  inserted(el) {
+    Prism.highlightAllUnder(el);
+  }
+});
 
 Vue.config.productionTip = false
 
